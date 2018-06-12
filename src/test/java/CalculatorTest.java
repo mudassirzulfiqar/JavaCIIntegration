65ed89dc436fb9cd17f5551b58a24d7a4038d7c3
Mudassir <mudaxxrex@gmail.com>
2018-06-13 00:05:46 +0500

    integrated maven into project

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by moodi on 13/06/2018.
+ */
+public class CalculatorTest {
+    @Test
+    public void sumShouldWork() {
+
+
+        Assert.assertEquals(4, Calculator.sum(2, 2));
+    }
+}
